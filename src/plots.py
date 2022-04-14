5cd7877c2c62518ad6e2d8a5e1b94bf70854ea17
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-04-14 13:52:08 +0300

    initial commit

--- src/plots.py
+++ src/plots.py
@@ -5,4 +5,4 @@ import networkx as nx
 def plot_graph(G):
     nx.draw(G, with_labels=True, font_weight='bold', node_color='red')
     plt.show()
-    plt.savefig("figures/graph.png")
+    plt.savefig("../figures/graph.png")
