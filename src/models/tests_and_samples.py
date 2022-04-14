ff2d6a53cb92222c9a2aa2e82ceeb7738ea0fe6b
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-04-14 15:49:54 +0300

    init

--- src/models/tests_and_samples.py
+++ src/models/tests_and_samples.py
@@ -1,4 +1,5 @@
-#%%
+#%% 
+# Graph (G), edges, and nodes initialization
 
 import networkx as nx
 from plots import Plots
@@ -20,6 +21,7 @@ print("Nodes and Edges: ",G.nodes(), ", ", G.edges())
 G.add_edge(*("A", "B"))
 print("Nodes and Edges: ",G.nodes(), ", ", G.edges())
 
+# Add multiple edges
 G.add_edges_from([("A","C"), ("B","D"), ("B","E"), ("C", "E")])
 print("Nodes and Edges: ",G.nodes(), ", ", G.edges())
 
@@ -32,18 +34,27 @@ print("Degree of A: ", G.degree("A"))
 # Print adjacency view
 print("Adjacency view: ", G.adj)
 
-#%%
-# Plot graph
-# print_me()
+#%% 
+# Graph plotting
+
 Plots.plot_graph(G)
 
 #%%
+# User initialization
+
 from user import User
 
-user = User("15234")
-user.get_id()
-user.set_visited_node("A")
-user.set_visited_node("B")
-user.get_visited_nodes()
+user1 = User("1")
+user1.get_id()
+user1.set_visited_node("A")
+user1.set_visited_node("B")
+user1.get_visited_nodes()
+
+user2 = User("1")
+user2.get_id()
+user2.set_visited_node("A")
+user2.set_visited_node("C")
+user2.set_visited_node("E")
+user2.get_visited_nodes()
 
 # %%
