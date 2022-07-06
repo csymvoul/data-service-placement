83d734e8fbccce7b4c4d9cc003d30c1cc11cb1da
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-07-06 14:26:46 +0300

    updated classes

--- src/models/edge.py
+++ src/models/edge.py
@@ -6,5 +6,18 @@ Edges specification
 """
 
 class Edge:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, latency=1, distance=1) -> None:
+        self.latency = latency
+        self.distance = distance
+
+    def set_latency(self, latency) -> None:
+        self.latency = latency
+
+    def get_latency(self) -> int:
+        return self.latency
+    
+    def set_distance(self, distance) -> None:
+        self.distance = distance
+
+    def get_distance(self) -> float:
+        return self.distance
--- src/models/node.py
+++ src/models/node.py
@@ -5,11 +5,21 @@ Node specification
     The Node class specifies a set of attributes that a node has.
 """
 class Node:
-    def __init__(self, lat:float, lon:float, response_time:int = 5, max_users:int = 10000) -> None:
+    def __init__(self, 
+                lat:float, 
+                lon:float, 
+                response_time:int = 5, 
+                max_users:int = 10000, 
+                cpu_cores:int = 2, 
+                memory:int = 8, 
+                capacity:int = 500) -> None:
         self.lat = lat
         self.long = lon
         self.response_time = response_time
         self.max_users = max_users
+        self.number_of_cpu_cores = cpu_cores
+        self.memory = memory
+        self.capacity = capacity
 
     def set_lan(self, lan:int) -> None:
         self.lan = lan
@@ -34,3 +44,21 @@ class Node:
 
     def get_max_users(self) -> None:
         return self.max_users
+
+    def set_number_of_cpu_cores(self, cpu_cores) -> None:
+        self.number_of_cpu_cores = cpu_cores
+    
+    def get_number_of_cpu_cores(self) -> int:
+        self.number_of_cpu_cores
+    
+    def set_memory(self, memory) -> None:
+        self.memory = memory
+
+    def get_memory(self) -> int:
+        return self.memory 
+    
+    def set_capacity(self, capacity) -> None:
+        self.capacity = capacity
+
+    def get_capacity(self) -> int:
+        return self.capacity 
