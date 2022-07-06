d4c6323948ccfedeaed4dccf18662cb081e1c811
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-07-06 14:02:45 +0300

    added data

--- src/models/user.py
+++ src/models/user.py
@@ -6,6 +6,7 @@ class User:
     def __init__(self, id) -> None:
         self.id = id
         self.visited_nodes = []
+        self.is_static = False
     
     def set_id(self, id:str) -> None:
         self.id = id
@@ -18,3 +19,9 @@ class User:
     
     def get_visited_nodes(self) -> list:
         return self.visited_nodes
+
+    def set_static(self, is_static) -> None:
+        self.is_static = is_static
+
+    def is_static(self) -> bool:
+        return self.is_static
