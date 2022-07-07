7a3481a6d12db27a61b8426206f9f3091803d2f5
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-07-07 12:53:42 +0300

    updates on scenario description

--- src/models/main.py
+++ src/models/main.py
@@ -46,6 +46,14 @@ class Simulation:
                     - edges with the following characteristics: 
                         - distance: calculated using the (lat,long) of the two nodes
                         - latency: would be in the this space [1, 10] - the greater the distance, the bigger the probability that latency will be higher
+                            - we find the min and max distances - split distances into 5 classes, where first class is the lowest distance space and the fifth the highest
+                                - if a distance belongs in the first class, it has a 80% probability the latency belong in the space [1,2], 18% belonging in [3, 8] and 2% of belonging in the [9,10]
+                                - similarly for the remaining classes: 
+                                    - second class: latency has 80% prob of being in the [3, 4] space, 5% of being in the [1,2] space, 10% of being in the [4,8], and 5% prob of being in the [9,10]
+                                    - third class: latency has 80% prob of being in the [5, 6] space, 5% of being in the [3,4] space, 10% of being in the [7,10], and 5% prob of being in the [1,2]
+                                    - fourth class: latency has 80% prob of being in the [7, 8] space, 5% of being in the [1,3] space, 10% of being in the [4,6], and 5% prob of being in the [9,10]
+                                    - fifth class: latency has 80% prob of being in the [9, 10] space, 2% of being in the [1,2] space, 10% of being in the [5,8], and 8% prob of being in the [3,4]
+                                    - once the probability space is selected, randomly select one of the values as latency
                 - create clusters of edge nodes, based on adjacency, that are comprised of maximum 10 nodes (at least 5 regions will be created)
                 - insert XXX users with the following characteristics 
                     - each data will have a data size: [1, 500]
@@ -59,10 +67,14 @@ class Simulation:
                     - cluster 2 will be mobile users
                     - cluster 3 will be static users
                         - for those users, the most used node will be identified as the optimal node
+                - placement and retrieval actions: 
+                    - 
                 - data to keep for each user: 
                     - list of visited nodes including the location of the nodes
+                    - edges between the nodes and their info (latency, distance, etc.)
                     - data sizes exchanged
                     - type of action (place or retrieval)
+                
         """
         pass
 
